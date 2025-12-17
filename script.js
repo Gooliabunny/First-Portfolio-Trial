0a6acc2e262f1b54fbd9626ff43d9f9f24a47b09
Tb <tbzadeh@gmail.com>
2025-12-17 21:36:35 +0400

    commit all

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-const scrollButtons = document.querySelectorAll('button[data-scrolll]');
+const scrollButtons = document.querySelectorAll('[data-scroll]');
+
+let scrolling = (button) => {
+    const targetId = button.getAttribute('data-scroll');
+    const targetElement = document.getElementById(targetId);
+
+    if (targetElement) {
+      targetElement.scrollIntoView({ behavior: 'smooth' });
+      setTimeout(() => {
+        targetElement.style.color = '#006600';
+      }, 100);
+      setTimeout(() => {
+        targetElement.style.color = '#d9d9d9';
+      }, 600);
+    }
+}
+
+scrollButtons.forEach(button => {
+  button.addEventListener('click', () => scrolling(button));
+});
